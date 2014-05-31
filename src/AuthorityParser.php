bb0b85821c32dec5ecef76cd7d3d7527c6ac9855
Dan Michael O. Heggø <danmichaelo@gmail.com>
2014-05-31 23:05:33 +0200

    Start working on Authority support

---
+++ src/AuthorityParser.php
@@ -0,0 +1,175 @@
+<?php namespace Scriptotek\SimpleMarcParser;
+
+use Danmichaelo\QuiteSimpleXmlElement\QuiteSimpleXmlElement;
+use Carbon\Carbon;
+
+class AuthorityParser {
+
+    // http://www.loc.gov/marc/authority/ad008.html
+    public static $cat_rules = array(
+        'a' => 'Earlier rules',
+        'b' => 'AACR 1',
+        'c' => 'AACR 2',
+        'd' => 'AACR 2 compatible',
+        'z' => 'Other',
+    );
+
+    public static $vocabularies = array(
+        'a' => 'lcsh',
+        'b' => 'lccsh', // LC subject headings for children's literature
+        'c' => 'mesh', // Medical Subject Headings
+        'd' => 'atg', // National Agricultural Library subject authority file (?)
+        'k' => 'cash', // Canadian Subject Headings
+        'r' => 'aat', // Art and Architecture Thesaurus 
+        's' => 'sears', // Sears List of Subject Heading 
+        'v' => 'rvm', // Répertoire de vedettes-matière
+    );
+
+    public function __construct() {
+
+    }
+
+    public function parse(QuiteSimpleXmlElement $record) {
+
+        $output = array();
+        $output['class'] = null;
+
+        // 001: Control number
+        $output['id'] = $record->text('marc:controlfield[@tag="001"]');
+
+        // 003: MARC code for the agency whose system control number is 
+        // contained in field 001 (Control Number)
+        // See http://www.loc.gov/marc/authority/ecadorg.html
+        $output['agency'] = $record->text('marc:controlfield[@tag="003"]');
+
+        // 005: Modified
+        $x = $record->text('marc:controlfield[@tag="005"]');
+        if (strlen($x) == 8) $output['modified'] = Carbon::createFromFormat('Ymd', $x);
+        if (strlen($x) == 16) $output['modified'] = Carbon::createFromFormat('YmdHis', substr($x,0,14)); // skip decimal fraction
+
+        // 008: Extract *some* information
+        $f008 = $record->text('marc:controlfield[@tag="008"]');
+        $r = substr($f008, 10, 1);
+        $output['cataloging'] = isset(self::$cat_rules[$r]) ? self::$cat_rules[$r] : null;
+        $r = substr($f008, 11, 1);
+        $output['vocabulary'] = isset(self::$vocabularies[$r]) ? self::$vocabularies[$r] : null;
+
+        $source = $record->first('marc:datafield[@tag="040"]');
+        if ($source) {
+            $output['catalogingAgency'] = $source->text('marc:subfield[@code="a"]') ?: null;
+            $output['language'] = $source->text('marc:subfield[@code="b"]') ?: null;
+            $output['transcribingAgency'] = $source->text('marc:subfield[@code="c"]') ?: null;
+            $output['modifyingAgency'] = $source->text('marc:subfield[@code="d"]') ?: null;
+            $output['vocabulary'] = $source->text('marc:subfield[@code="f"]') ?: $output['vocabulary'];            
+        }
+
+
+        // 100: Personal name (NR)
+        foreach ($record->xpath('marc:datafield[@tag="100"]') as $field) {
+            $output['class'] = 'person';
+            $output['name'] = $field->text('marc:subfield[@code="a"]');
+            $spl = explode(', ', $output['name']);
+            if (count($spl) == 2) {
+                $output['name'] = $spl[1] . ' ' . $spl[0];
+            }
+            $bd = $field->text('marc:subfield[@code="d"]');
+            $bd = explode('-', $bd);
+            $output['birth'] = $bd[0] ?: null;
+            $output['death'] = (count($bd) > 0 && $bd[1]) ? $bd[1] : null;
+        }
+
+        // 110: Corporate Name (NR)
+        foreach ($record->xpath('marc:datafield[@tag="110"]') as $field) {
+            $output['class'] = 'corporate';
+            // TODO: ...
+        }
+
+        // 111: Meeting Name (NR)
+        foreach ($record->xpath('marc:datafield[@tag="111"]') as $field) {
+            $output['class'] = 'meeting';
+            // TODO: ...
+        }
+
+        // 130: Uniform title: Not interested for now
+
+        // 150: Topical Term (NR)
+        foreach ($record->xpath('marc:datafield[@tag="150"]') as $field) {
+            $output['class'] = 'topicalTerm';
+            $output['term'] = $field->text('marc:subfield[@code="a"]');
+            $name = $field->text('marc:subfield[@code="a"]');
+            foreach ($field->xpath('marc:subfield[@code="x"]') as $s) { 
+                $name .= ' : ' . $s;
+            }
+            foreach ($field->xpath('marc:subfield[@code="v"]') as $s) {
+                $name .= ' : ' . $s;
+            }
+            foreach ($field->xpath('marc:subfield[@code="y"]') as $s) {
+                $name .= ' : ' . $s;
+            }
+            foreach ($field->xpath('marc:subfield[@code="z"]') as $s) {
+                $name .= ' : ' . $s;
+            }
+            $output['name'] = $name;
+            // TODO: ...
+        }
+
+        // 151: Geographic Term (NR)
+        // 155: Genre/form Term (NR)
+
+        // 375: Gender (R)
+        $output['genders'] = array();
+        foreach ($record->xpath('marc:datafield[@tag="375"]') as $field) {
+            $gender = $field->text('marc:subfield[@code="a"]');
+            $start = $field->text('marc:subfield[@code="s"]');
+            $end = $field->text('marc:subfield[@code="e"]');
+            $output['genders'][] = array(
+                'value' => $gender,
+                'from' => $start,
+                'until' => $end,
+            );
+        }
+        // Alias gender to the last value to make utilizing easier
+        $output['gender'] = (count($output['genders']) > 0)
+            ? $output['genders'][count($output['genders']) - 1]['value']  // assume sane ordering for now
+            : null;
+
+
+        // TODO: rest
+
+        return $output;
+    }
+
+}
+
+/*
+<?xml version="1.0"?>
+<recordData>
+    <marc:record xmlns:marc="info:lc/xmlns/marcxchange-v1" format="MARC21" type="Authority">
+    <marc:leader>99999nz a2299999n 4500</marc:leader>
+    <marc:controlfield tag="001">x90531735</marc:controlfield>
+    <marc:controlfield tag="003">NO-TrBIB</marc:controlfield>
+    <marc:controlfield tag="005">20090407000000.0</marc:controlfield>
+    <marc:controlfield tag="008">090407n adznnaabn| |a|ana| </marc:controlfield>
+    <marc:datafield tag="016" ind1="7" ind2=" ">
+        <marc:subfield code="a">x90531735</marc:subfield>
+        <marc:subfield code="2">NO-TrBIB</marc:subfield>
+    </marc:datafield>
+    <marc:datafield tag="040" ind1=" " ind2=" ">
+        <marc:subfield code="a">NO-OsNB</marc:subfield>
+        <marc:subfield code="b">nob</marc:subfield>
+        <marc:subfield code="c">NO-TrBIB</marc:subfield>
+        <marc:subfield code="f">noraf</marc:subfield>
+    </marc:datafield>
+    <marc:datafield tag="100" ind1="1" ind2=" ">
+        <marc:subfield code="a">Bakke, Dagfinn</marc:subfield>
+        <marc:subfield code="d">1933-</marc:subfield>
+    </marc:datafield>
+    <marc:datafield tag="375" ind1=" " ind2=" ">
+        <marc:subfield code="a">male</marc:subfield>
+    </marc:datafield>
+    <marc:datafield tag="400" ind1="0" ind2=" ">
+        <marc:subfield code="a">DAN</marc:subfield>
+    </marc:datafield>
+</marc:record>
+</recordData>
+*/
---
+++ tests/AuthorityParserTest.php
@@ -0,0 +1,146 @@
+<?php namespace Scriptotek\SimpleMarcParser;
+
+require 'vendor/autoload.php';
+use Danmichaelo\QuiteSimpleXMLElement\QuiteSimpleXMLElement;
+use Scriptotek\SimpleMarcParser\AuthorityParser;
+use Carbon\Carbon;
+
+class AuthorityParserTest extends \PHPUnit_Framework_TestCase {
+
+    // http://sru.bibsys.no/search/authority?version=1.2&operation=searchRetrieve&startRecord=1&maximumRecords=10&query=rec.identifier%3D%22x90061718%22&recordSchema=marcxchange
+    // http://sru.bibsys.no/search/authority?version=1.2&operation=searchRetrieve&startRecord=1&maximumRecords=10&query=rec.identifier%3D%22x13038487%22&recordSchema=marcxchange
+
+    private function parseRecordData($data)
+    {
+        $dom = new QuiteSimpleXMLElement('<?xml version="1.0"?>
+            <marc:record xmlns:marc="info:lc/xmlns/marcxchange-v1" format="MARC21" type="Authority">
+                ' . $data . '
+            </marc:record>');
+        $dom->registerXPathNamespaces(array(
+            'marc' => 'http://www.loc.gov/MARC21/slim'
+        ));
+
+        $parser = new AuthorityParser;
+        return $parser->parse($dom);
+    }
+
+    public function testMarc001() {
+        $out = $this->parseRecordData('
+            <marc:controlfield tag="001">12149361x</marc:controlfield>
+        ');
+
+        $this->assertEquals('12149361x', $out['id']);
+    }
+
+    public function testMarc003() {
+        $out = $this->parseRecordData('
+           <marc:controlfield tag="003">NO-TrBIB</marc:controlfield>
+        ');
+
+        $this->assertEquals('NO-TrBIB', $out['agency']);
+    }
+
+    public function testMarc005() {
+        $out = $this->parseRecordData('
+           <marc:controlfield tag="005">20090407000000.0</marc:controlfield>
+        ');
+
+        $this->assertEquals(
+            Carbon::create(2009, 4, 7, 0, 0, 0), 
+            $out['modified']
+        );
+    }
+
+    public function testMarc008() {
+        $out = $this->parseRecordData('
+           <marc:controlfield tag="008">090407n adznnaabn| |a|ana| </marc:controlfield>
+        ');
+
+        $this->assertEquals('Other', $out['cataloging']);
+        $this->assertEquals(null, $out['vocabulary']);
+    }
+
+    // 040 - Cataloging Source (NR)
+    public function testMarc040() {
+        $out1 = $this->parseRecordData('
+           <marc:datafield tag="040" ind1=" " ind2=" ">
+              <marc:subfield code="a">NO-OsNB</marc:subfield>
+              <marc:subfield code="b">nob</marc:subfield>
+              <marc:subfield code="c">NO-TrBIB</marc:subfield>
+              <marc:subfield code="f">noraf</marc:subfield>
+           </marc:datafield>
+        ');
+
+        $this->assertEquals('NO-OsNB', $out1['catalogingAgency']);
+        $this->assertEquals('nob', $out1['language']);
+        $this->assertEquals('NO-TrBIB', $out1['transcribingAgency']);
+        $this->assertNull($out1['modifyingAgency']);
+        $this->assertEquals('noraf', $out1['vocabulary']);
+    }
+
+    // 100 - Heading-Personal Name (NR)
+    public function testMarc100() {
+        $out1 = $this->parseRecordData('
+           <marc:datafield tag="100" ind1="1" ind2=" ">
+              <marc:subfield code="a">Bakke, Dagfinn</marc:subfield>
+              <marc:subfield code="d">1933-</marc:subfield>
+           </marc:datafield>
+        ');
+        $out2 = $this->parseRecordData('
+           <marc:datafield tag="100" ind1="1" ind2=" ">
+              <marc:subfield code="a">Ibsen, Henrik</marc:subfield>
+              <marc:subfield code="d">1828-1906</marc:subfield>
+           </marc:datafield>
+        ');
+
+        $this->assertEquals('person', $out1['class']);
+        $this->assertEquals(1933, $out1['birth']);
+        $this->assertNull($out1['death']);
+        $this->assertEquals('Dagfinn Bakke', $out1['name']);
+
+        $this->assertEquals(1828, $out2['birth']);
+        $this->assertEquals(1906, $out2['death']);
+    }
+
+    // 150 - Topical Term (NR)
+    public function testMarc150() {
+        $out1 = $this->parseRecordData('
+           <marc:datafield tag="150" ind1=" " ind2=" ">
+              <marc:subfield code="a">Fotomikrografi</marc:subfield>
+              <marc:subfield code="x">naturvitenskap</marc:subfield>
+           </marc:datafield>
+        ');
+
+        $this->assertEquals('topicalTerm', $out1['class']);
+        $this->assertEquals('Fotomikrografi : naturvitenskap', $out1['name']);
+    }
+
+    // 375 - Gender (R)
+    public function testMarc375() {
+        $out1 = $this->parseRecordData('
+           <marc:datafield tag="375" ind1=" " ind2=" ">
+              <marc:subfield code="a">male</marc:subfield>
+              <marc:subfield code="s">1926</marc:subfield>
+              <marc:subfield code="e">1972?</marc:subfield>
+           </marc:datafield>
+           <marc:datafield tag="375" ind1=" " ind2=" ">
+              <marc:subfield code="a">female</marc:subfield>
+              <marc:subfield code="s">1972?</marc:subfield>
+           </marc:datafield>
+        ');
+
+        $this->assertCount(2, $out1['genders']);
+        $this->assertEquals('male', $out1['genders'][0]['value']);
+        $this->assertEquals('1926', $out1['genders'][0]['from']);
+        $this->assertEquals('1972?', $out1['genders'][0]['until']);
+        $this->assertEquals('female', $out1['genders'][1]['value']);
+        $this->assertEquals('1972?', $out1['genders'][1]['from']);
+        $this->assertEquals('female', $out1['gender']);
+
+        $out2 = $this->parseRecordData('');
+
+        $this->assertCount(0, $out2['genders']);
+        $this->assertNull($out2['gender']);
+    }
+
+}
